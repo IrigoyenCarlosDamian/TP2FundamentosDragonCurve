cfe759e8aaf546fd7d5adce057da93d96f8acecf
cIrigoyen16 <cyrigoyen969@gmail.com>
2022-11-12 17:13:15 -0300

    se agrego la libreria click para recibir como argumento del programa la cantidad de iteracciones hay que definir que otros parameteros se van a enviar por la linea de comandos,falta agregar la tortuga

--- curva_de_dragon.py
+++ curva_de_dragon.py
@@ -1,23 +1,38 @@
+import click
+import turtle
+from random import randrange
+from click.types import INT, STRING, IntParamType, IntRange
+
 reglas = {
     'X':'X+YF+',
     'Y':'-FX-Y'
 }
+
 def procesar_cadena(iteraciones):
-    resultado = 'FX'
+    resultado = 'FX' #iteraccion 0
     for i in range(0, iteraciones):
         for caracter in resultado:
             if caracter in reglas:
                 resultado += reglas[caracter]
-                print(resultado)
             else:
                 resultado += caracter
+           
     return resultado
 
-def inicializar():
-    cadena=''
-    cadena+=procesar_cadena(10)
-    input('\nPresione ENTER para mostrar la cadena.')
-    print(cadena)
+# Parametros que puede recibir el programa(por ahora solo la cantidad de iteracciones definir que otros parametros se va a enviar)
+@click.command()
+@click.option('-i', '--iteraciones', 
+                required=True, 
+                type=click.IntRange(1, 10, clamp=True), 
+                help='Cantidad de iteraciones (1..10).')
 
+def inicio(iteraciones):
+    print('''Parametros de ejecucion: 
+    -> Cantidad de iteraciones: {}'''
+    .format(iteraciones))
+    cadena=''
+    cadena+=procesar_cadena(iteraciones)
+    print(cadena)
+    print(len(cadena))
 if __name__ == '__main__':
-    inicializar()
+    inicio()
