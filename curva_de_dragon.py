6e475ef221a2169349d2c74e28626bef47da6d52
cIrigoyen16 <cyrigoyen969@gmail.com>
2022-11-13 17:17:00 -0300

    se agrego la libreria  tortuga ya hace el grafico de la cadena inicial y de algunas de su derivaciones toca pulir mas los parametros de entrada y hacerlo mas generico

--- curva_de_dragon.py
+++ curva_de_dragon.py
@@ -7,9 +7,45 @@ reglas = {
     'X':'X+YF+',
     'Y':'-FX-Y'
 }
+bgclor="black"
+color="green"
+angulo=90
+segmento=8
+
+def graficar(cadena):
+    # Creo una instancia de turtle
+    ventana = turtle.Screen()
+    ventana.setup(width=1.0, height=1.0, startx=None, starty=None)
+    ventana.title('Curva De Dragon')
+    ventana.screensize(1920 * 3, 1080 * 3)
+    ventana.setup(width=1.0, height=1.0, startx=None, starty=None)
+    tortuga = turtle.Turtle()
+    tortuga.screen.bgcolor(bgclor)
+    tortuga.color(color)
+    tortuga.speed(200)
+    try:
+        for simbolo in cadena:
+            if simbolo=='F': #DIBUJO UN SEGMENTO
+                tortuga.forward(segmento)
+                
+            elif simbolo=='+': #ME MUEVO A LA DRECHA EN UN ANGULO DE 90
+                tortuga.right(angulo)
+
+            elif simbolo=='-': #ME MUEVO A LA IZQUIERDA EN UN ANGULO DE 90
+                tortuga.left(angulo)
+            else:
+                turtle.backward(segmento)
+
+    except Exception as e:
+        print("algo paso")
+    finally:
+        print("la tortuga se canso de caminar y desidio terminar el programa")
+
+
+
 
 def procesar_cadena(iteraciones):
-    resultado = 'FX' #iteraccion 0
+    resultado = 'FX' #iteracion 0
     for i in range(0, iteraciones):
         for caracter in resultado:
             if caracter in reglas:
@@ -32,7 +68,6 @@ def inicio(iteraciones):
     .format(iteraciones))
     cadena=''
     cadena+=procesar_cadena(iteraciones)
-    print(cadena)
-    print(len(cadena))
+    graficar(cadena)
 if __name__ == '__main__':
     inicio()
